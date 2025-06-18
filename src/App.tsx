030e662779882d06f2600b491fc954b6d4f1f1e6
aboudev22 <aboubakarjay2@gmail.com>
2025-06-18 14:24:32 +0100

    addind screenshot

--- src/App.tsx
+++ src/App.tsx
@@ -2,49 +2,33 @@ import { AnimatePresence, motion } from "framer-motion";
 import { useState } from "react";
 
 export default function App() {
-  const [clicked, setClicked] = useState<number | null>(null);
-
+  const [selected, setSelected] = useState<number | null>(null);
   return (
-    <div className="w-screen h-screen flex flex-col justify-center items-center relative overflow-hidden bg-gray-100">
-      <div className="relative flex flex-wrap justify-around items-center p-2 bg-blue-300 h-96 w-96 rounded-md">
-        {items.map((item, i) => (
+    <div className="w-screen h-screen bg-green-900 flex justify-center items-center">
+      <div className="p-5 w-96 flex flex-wrap justify-center items-center rounded-md gap-2 bg-green-800 border-2 border-green-500">
+        {images.map((item, index) => (
           <motion.img
-            layoutId={`img-${i}`}
-            key={item.title}
+            layoutId={`img-${index}`}
             src={item.src}
-            alt={item.title}
-            onClick={() => setClicked(i)}
-            className="w-36 h-36 rounded-md cursor-pointer object-cover"
+            onClick={() => setSelected(index)}
+            className="w-36 h-36 rounded-md cursor-pointer"
           />
         ))}
       </div>
-
       <AnimatePresence>
-        {clicked !== null && (
+        {selected !== null && (
           <motion.div
-            layout
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            transition={{ duration: 0.4 }}
-            className="fixed flex justify-center items-center bg-neutral-500/50 top-0 bottom-0 w-full"
-            onClick={() => setClicked(null)}
+            onClick={() => setSelected(null)}
+            className="fixed top-0 bottom-0 w-full flex justify-center items-center bg-neutral-500/20"
           >
-            <motion.div
-              layoutId={`img-${clicked}`}
-              onClick={() => setClicked(null)}
-              className="flex flex-col"
-            >
-              <motion.img
-                src={items[clicked].src}
-                alt={items[clicked].title}
-                onClick={() => setClicked(null)}
-                className="w-64 h-64 cursor-pointer"
-              />
-              <p className="bg-black text-xs text-white w-full p-2">
-                description de l'image
-              </p>
-            </motion.div>
+            <motion.img
+              layoutId={`img-${selected}`}
+              src={images[selected].src}
+              className="w-60 h-60 cursor-pointer"
+            />
           </motion.div>
         )}
       </AnimatePresence>
@@ -53,12 +37,12 @@ export default function App() {
 }
 
 /**
- * ================= Data ==================
+ * ======================= Data ===========================
  */
 
-const items = [
-  { src: "/kakashi.jpg", title: "Kakashi" },
-  { src: "/leo-messi.jpg", title: "Leo Messi" },
-  { src: "/leo-messi.jpg", title: "Leo Messi 2" },
-  { src: "/kakashi.jpg", title: "Kakashi2" },
+const images = [
+  { src: "/kakashi.jpg", name: "kakashi" },
+  { src: "/leo-messi.jpg", name: "leo" },
+  { src: "/leo-messi.jpg", name: "leo" },
+  { src: "/kakashi.jpg", name: "kakashi" },
 ];
