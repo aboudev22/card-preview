845637eadce462ff902022f2d775fd40fbb7391d
aboudev22 <aboubakarjay2@gmail.com>
2025-06-19 11:40:25 +0100

    refactoring code

--- src/App.tsx
+++ src/App.tsx
@@ -3,46 +3,43 @@ import { useState } from "react";
 
 export default function App() {
   const [selected, setSelected] = useState<number | null>(null);
+  const card = [
+    { id: 1, src: "/kakashi.jpg", name: "Kakashi" },
+    { id: 2, src: "/leo-messi.jpg", name: "Kakashi" },
+    { id: 3, src: "/leo-messi.jpg", name: "Kakashi" },
+    { id: 4, src: "/kakashi.jpg", name: "Kakashi" },
+  ];
   return (
-    <div className="w-screen h-screen bg-green-900 flex justify-center items-center">
-      <div className="p-5 w-96 flex flex-wrap justify-center items-center rounded-md gap-2 bg-green-800 border-2 border-green-500">
-        {images.map((item, index) => (
+    <div className="w-screen h-screen flex justify-center items-center bg-transparent">
+      <div className="grid grid-cols-2 grid-rows-2 gap-2">
+        {card.map((item, i) => (
           <motion.img
-            layoutId={`img-${index}`}
+            layoutId={`img-${i}`}
             src={item.src}
-            onClick={() => setSelected(index)}
-            className="w-36 h-36 rounded-md cursor-pointer"
+            key={item.id}
+            className="w-52 h-52 rounded-md cursor-pointer"
+            onClick={() => setSelected(i)}
           />
         ))}
+        <AnimatePresence>
+          {selected !== null && (
+            <motion.section
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              exit={{ opacity: 0 }}
+              onClick={() => setSelected(null)}
+              className="fixed top-0 bottom-0 left-0 right-0 bg-neutral-900/10 flex justify-center items-center"
+            >
+              <motion.img
+                onClick={() => setSelected(null)}
+                src={card[selected].src}
+                layoutId={`img-${selected}`}
+                className="w-72 h-72 object-cover cursor-pointer"
+              />
+            </motion.section>
+          )}
+        </AnimatePresence>
       </div>
-      <AnimatePresence>
-        {selected !== null && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            onClick={() => setSelected(null)}
-            className="fixed top-0 bottom-0 w-full flex justify-center items-center bg-neutral-500/20"
-          >
-            <motion.img
-              layoutId={`img-${selected}`}
-              src={images[selected].src}
-              className="w-60 h-60 cursor-pointer"
-            />
-          </motion.div>
-        )}
-      </AnimatePresence>
     </div>
   );
 }
-
-/**
- * ======================= Data ===========================
- */
-
-const images = [
-  { src: "/kakashi.jpg", name: "kakashi" },
-  { src: "/leo-messi.jpg", name: "leo" },
-  { src: "/leo-messi.jpg", name: "leo" },
-  { src: "/kakashi.jpg", name: "kakashi" },
-];
